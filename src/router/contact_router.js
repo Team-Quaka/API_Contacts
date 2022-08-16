eb21554900be6352c2249908ea2f6b597ba11454
John <rezamosavian1014@gmail.com>
2022-08-16 18:20:10 +0430

    feat:add mock data for get contacts

--- src/router/contact_router.js
+++ src/router/contact_router.js
@@ -7,7 +7,7 @@ contactRouter.post('/add-contact', async (req, res) => {
 });
 
 contactRouter.get('/get-contacts', async (req, res) => {
-    return res.status(200).json();
+    return res.status(200).json({ 'contacts': [{ 'firstname': 'reza', 'lastname': 'mous', 'phonenumber': '09109947180' }, { 'firstname': 'reza', 'lastname': 'mous', 'phonenumber': '09109947180' }, { 'firstname': 'reza', 'lastname': 'mous', 'phonenumber': '09109947180' }, { 'firstname': 'reza', 'lastname': 'mous', 'phonenumber': '09109947180' }], 'page': 1 });
 });
 
 contactRouter.put('/edit-contact', async (req, res) => {
