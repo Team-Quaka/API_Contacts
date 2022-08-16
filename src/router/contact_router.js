cf423e6b2396632af178fc99c37bbd5e89d1bf20
Ali Shafiee <shafiee.ali10@gmail.com>
2022-08-16 16:24:24 +0430

    Add slash to all contact routers

--- src/router/contact_router.js
+++ src/router/contact_router.js
@@ -2,15 +2,15 @@ const express = require('express');
 const contactRouter = express.Router();
 
 
-contactRouter.post('add-contact', async (req, res) => {
+contactRouter.post('/add-contact', async (req, res) => {
     return res.status(200).json();
 });
 
-contactRouter.get('get-contacts', () => {
+contactRouter.get('/get-contacts', () => {
     return res.status(200).json();
 });
 
-contactRouter.put('edit-contact', (req, res) => {
+contactRouter.put('/edit-contact', (req, res) => {
     return res.status(200).json();
 
 });
