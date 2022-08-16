8e1423d7bcd1475ceb7e3c0b5fb7dc7749cb43e4
John <rezamosavian1014@gmail.com>
2022-08-16 21:22:35 +0430

    feat :add connection to mongodb

--- src/model/index.js
+++ src/model/index.js
@@ -1,15 +1,19 @@
-// const mongoose = require('mongoose');
-// mongodb_url_connection = "mongodb+srv://admin:admin@sharpdb.2nmzj.mongodb.net/?retryWrites=true&w=majority;;";
+const mongoose = require('mongoose');
+const username = process.env.dbusername | 'admin';
+const password = process.env.dbpassword;
+mongodb_url_connection = `mongodb+srv://${username}:${password}@sharpdb.2nmzj.mongodb.net/?retryWrites=true&w=majority;`;
 
 
-// try {
-//     console.log('start connectiong to DB');
-//     mongoose.connect(mongodb_url_connection, {
-//         useNewUrlParser: true,
-//         useCreateIndex: true,
-//         useFindAndModify: false
-//     });
-//     console.log('connected to DB successfully');
-// } catch (e) {
-//     console.error(e);
-// }
+console.log('start connectiong to DB');
+mongoose.connect(mongodb_url_connection, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+});
+console.log('connected to DB successfully');
+
+//Get the default connection
+const db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
