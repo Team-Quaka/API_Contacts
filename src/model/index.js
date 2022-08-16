108b0d9d19909917f9e034b861d6d040a265725e
John <rezamosavian1014@gmail.com>
2022-08-16 18:24:33 +0430

    fix

--- src/model/index.js
+++ src/model/index.js
@@ -1,15 +1,15 @@
-const mongoose = require('mongoose');
-mongodb_url_connection = "mongodb+srv://admin:admin@sharpdb.2nmzj.mongodb.net/?retryWrites=true&w=majority;;";
+// const mongoose = require('mongoose');
+// mongodb_url_connection = "mongodb+srv://admin:admin@sharpdb.2nmzj.mongodb.net/?retryWrites=true&w=majority;;";
 
 
-try {
-    console.log('start connectiong to DB');
-    mongoose.connect(mongodb_url_connection, {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useFindAndModify: false
-    });
-    console.log('connected to DB successfully');
-} catch (e) {
-    console.error(e);
-}
+// try {
+//     console.log('start connectiong to DB');
+//     mongoose.connect(mongodb_url_connection, {
+//         useNewUrlParser: true,
+//         useCreateIndex: true,
+//         useFindAndModify: false
+//     });
+//     console.log('connected to DB successfully');
+// } catch (e) {
+//     console.error(e);
+// }
